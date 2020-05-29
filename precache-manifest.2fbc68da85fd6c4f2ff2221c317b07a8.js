96ae16fff66872460c7b352c4db10aa26da6b9bf
umbaomart <umbaomart@gmail.com>
2020-05-29 15:42:00 +0800

    Updates

---
+++ precache-manifest.2fbc68da85fd6c4f2ff2221c317b07a8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c20ad5069f0a7f14e609be240baa08ac",
+    "url": "/awwwards1reactplusgsap/index.html"
+  },
+  {
+    "revision": "4fa1c9f06f3f4c7efb5e",
+    "url": "/awwwards1reactplusgsap/static/css/main.d66ed833.chunk.css"
+  },
+  {
+    "revision": "c422d8f83a6a256ea89a",
+    "url": "/awwwards1reactplusgsap/static/js/2.7aadb8e4.chunk.js"
+  },
+  {
+    "revision": "3cd4b6bc4f51e64816d8a2b69bba1790",
+    "url": "/awwwards1reactplusgsap/static/js/2.7aadb8e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fa1c9f06f3f4c7efb5e",
+    "url": "/awwwards1reactplusgsap/static/js/main.2ebf83e5.chunk.js"
+  },
+  {
+    "revision": "d99539adcd539b3091de",
+    "url": "/awwwards1reactplusgsap/static/js/runtime-main.1787f22d.js"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/awwwards1reactplusgsap/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/awwwards1reactplusgsap/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "e84c205576ce51cc6a90653c49df11f0",
+    "url": "/awwwards1reactplusgsap/static/media/curology-min.e84c2055.png"
+  },
+  {
+    "revision": "379bad944ff3f73142989e87c5b24773",
+    "url": "/awwwards1reactplusgsap/static/media/lumin-min.379bad94.png"
+  },
+  {
+    "revision": "5f9e30771ee4dbbad1689af16cb49028",
+    "url": "/awwwards1reactplusgsap/static/media/yourspace-min.5f9e3077.png"
+  }
+]);
